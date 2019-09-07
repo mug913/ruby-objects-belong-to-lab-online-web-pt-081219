5d28a5bd9bafa3a6d9fba621f30676519c822e24
Brad Alexander <brad.alexander913@gmail.com>
2019-09-07 00:09:26 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,10 @@
+require 'pry'
+
 class Post 
-  
   attr_accessor :title 
-  
   def initalize(title)
     @title = title 
   end 
-
 end
 
 
@@ -13,3 +12,4 @@ author = Author.new("Drake")
 posting = Post.new("Hotline Bling")
  
 posting.Author = author
+binding.pry
